f751b5cae7b098ebea2ab79859066d0c4788dcbb
IzzJuan <juangarciavxx@gmail.com>
2021-04-05 17:17:48 -0500

    Generando sistema de archivos y alistando los frameworks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
         <div className="App">
             <Router>
                 <Switch>
-                    <Route path='/' render={() => {
+                    <Route exact path='/' render={() => {
                         return <header className="App-header">
                             <p>
                                 Edit <code>src/App.js</code> and save to reload.
